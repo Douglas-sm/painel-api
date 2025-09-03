1561850f4db1ede67e820cec10ab07004631c476
Douglas SM <douglas.stmr@gmail.com>
2025-09-03 14:01:18 -0300

    feat: add questions hub routes and database configuration

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\QuestionsHubController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,13 @@ Route::post('/updateTheme', [\App\Http\Controllers\ThemeController::class, 'upda
 Route::post('/tenant_register', [\App\Http\Controllers\TenantController::class, 'register']);
 Route::get('/tenants', [\App\Http\Controllers\TenantController::class, 'index']);
 Route::post('/tenants/{id}/delete', [\App\Http\Controllers\TenantController::class, 'destroy']);
+
+// Questions Hub routes
+Route::get('/questions-hub/collections', [QuestionsHubController::class, 'getCollections']);
+Route::get('/questions-hub/collections/{id}/questions', [QuestionsHubController::class, 'getCollectionWithQuestions']);
+Route::get('/questions-hub/questions', [QuestionsHubController::class, 'getQuestions']);
+Route::get('/questions-hub/questions/{id}', [QuestionsHubController::class, 'getQuestionWithChoices']);
+Route::get('/questions-hub/collections/{collectionId}/questions/all', [QuestionsHubController::class, 'getQuestionsByCollection']);
 
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
